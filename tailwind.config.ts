f90c359303da19392183f933e89b36603bf9b939
t-naganuma <takuya.naganuma@gmail.com>
2024-09-23 18:38:35 +0900

    init settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		colors: {
+			primary: "#E3D9C7",
+			secondary: "#FFF8F1",
+			buttonColor: "#9E8F6E",
+			black: "#333333",
+			danger: "#BA1A1A",
+			brown: "#755B00",
+			primaryDarken: "#ccc3b3",
+			surface: "#FFF8F1",
+			white: "#FFFFFF",
+			gray: "#808080",
+		},
+	},
+	plugins: [],
 };
 export default config;
